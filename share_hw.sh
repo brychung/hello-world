33f8c32d1a79fd0fce04ee5b7fa35c431ff14708
mmillervedam <mmillervedam@users.noreply.github.com>
2016-02-03 22:43:14 -0500

    updated bash  script with Scott's help

--- share_hw.sh
+++ share_hw.sh
@@ -1,38 +1,48 @@
-# This file is an attempt to make a bash script to automate the process
-# for sharing past homework via GitHub. Feel free to download and adjust
-# the global variables to your own situation.
-#
-# I am also looking for help in generalizing the process. Ideally the
-# script would be something that I could use week to week with out editing
-# it each time. In order for that to be the case we need a way to solicit
-# the week # from the user. I don't know how to do this in bash...
+# This bash script automates the process for sharing past homework via GitHub.
+# The script assumes you have already cloned the homework-share repo and copies
+# your homework into the appropriate folder. It does NOT push up to Git.
 
-#############################################################################
+# Thank you to Scott who pointed me to the read statements and conditionals!
 
-# STEP 0: Somehow solicit the week # and folder paths... ???? (I don't know how
-# to do this so for now I'm just making them global variables. It would be great
-# if we could just append the name of the week to the end of the general folder)
+# STEP 0: Welcome message then solicit user input for folder paths and week #
+echo "... Starting process to share homework."
+echo "On the next line please enter the folder path for the directory where you \
+cloned your studentXXX repo. Your path should NOT end with / or /week_# "
+read -p ">>> Enter the path here:" ORIGINAL_LOC
+echo ""
 
-# path for HW Submission folder (original location of files)
-ORIGINAL_LOC = /Users/mmillervedam/Documents/MIDS/python_bridge_course/HW_Assignments/week_1
-# path for HW Sharing folder (destination for shared files)
-DESTINATION = /Users/mmillervedam/Documents/MIDS/python_bridge_course/homework-share/week_1
+echo "Next enter the folder path for the directory where you cloned the homework-share \
+repo. Just like before, your path should not end with / and should not include the week_# folder."
+read -p ">>> Enter the path here:" DESTINATION
+echo ""
 
-# STEP 1: cd into folder where I save and submit my hw for that week.
-cd $ORIGINAL_LOC
+echo "Finally, which week's homework do you want to share?"
+read -p ">>> Enter the number here:" WEEK
+read -p ">>> and your GitHub username here:" USERNAME
+echo ""
 
-# STEP 2: use ls to get a list of files in the folder
-file_names=$( ls )
+# STEP 1: perform git pull to update share folder and prevent merge conflicts
+cd $DESTINATION
+echo "... performing git pull."
+git pull
 
-# STEP 3: loop through list copying each file to the location of the share folder
-# (The internet suggests that this is what a bash loop should look like, IDK...)
-for file in $file_names; do
-    cp $file $DESTINATION/$file #I'm not sure this is going to work how I want it to
-done
+# STEP 2: copy files in to new folder
+mkdir week_$WEEK/$USERNAME
+echo "... copying files."
+cp $ORIGINAL_LOC/week_$WEEK/* $DESTINATION/week_$WEEK/$USERNAME/
 
-# STEP 4: cd into share folder
-cd  $DESTINATION
-
-# STEP 5: git pull, git push to upload?
-# ...... or maybe the pull should have happened earlier?
-# ...... or maybe we should let the user do this step on their own outside the script?
+# STEP 3: commit changes (credit: Scott for conditional sequence, Thank You!)
+echo "... Files copied. Ready to commit."
+echo "... NOTE: all changes to share folder will be committed."
+read -p ">>> Proceed with commit? (y/n):" CONTINUE
+if [[ $CONTINUE = "y" ]]
+then
+    echo "... committing changes."
+    git add .
+	git commit -m "sharing week $WEEK HW ( $USERNAME )"
+    echo "... files committed, but not pushed."
+    echo "HW ready to share, just push them up when you are ready."
+else
+	echo "... OK - files moved, but not committed."
+    echo "HW ready to share, commit and push when you are ready."
+fi
