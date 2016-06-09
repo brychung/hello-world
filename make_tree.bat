eba17d59f7de0c061d3e83e05c743840b01ab639
brychung <brychung@hotmail.com>
2016-06-09 00:31:09 -0700

    Y

---
+++ make_tree.bat
@@ -0,0 +1,32 @@
+echo on
+
+cd\
+cd /c/Users/"Bryan Home"/BRYCHUNG/homework-share-SS2016/
+
+mkdir s1
+cd s1
+
+mkdir s3/conf.txt
+touch conf.txt
+echo "I love bash scripting." >> conf.txt
+
+mkdir s2/text_chunk1.txt
+touch text_chunk1.txt
+echo "A whole new world
+A new fantastic point of view
+No one to tell us no
+Or where to go
+Or say we're only dreaming" >> text_chunk1.txt
+
+mkdir Advanced
+cd Advanced
+
+cd ..
+
+cp text_chunk1.txt Advanced/text_chunk2.txt
+
+echo "A whole new world
+A dazzling place I never knew
+But when I'm way up here
+It's crystal clear
+That now I'm in a whole new world with you" >> text_chunk2.txt
