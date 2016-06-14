4fad4723996f908938bb6a13984e6fe0bce4a6cd
Ikya <ikandula@berkeley.edu>
2016-06-13 21:55:11 -0700

    first commit

---
+++ make_tree.sh
@@ -0,0 +1,16 @@
+Ikyas-MacBook-Air:assignment-upstream-SS ikyakandula$ mkdir -p s1/s3
+Ikyas-MacBook-Air:assignment-upstream-SS ikyakandula$ mkdir -p s1/s2
+Ikyas-MacBook-Air:assignment-upstream-SS ikyakandula$ mkdir -p s1/s2/Advanced
+Ikyas-MacBook-Air:assignment-upstream-SS ikyakandula$ touch s1/s3/conf.txt
+Ikyas-MacBook-Air:assignment-upstream-SS ikyakandula$ touch s1/s2/text_chunk.txt
+Ikyas-MacBook-Air:assignment-upstream-SS ikyakandula$ touch s1/s2/Advanced/text_chunk2.txt
+Ikyas-MacBook-Air:assignment-upstream-SS ikyakandula$ echo "I love bash scripting." >> s1/s3/conf.txt
+Ikyas-MacBook-Air:assignment-upstream-SS ikyakandula$ git add s1
+Ikyas-MacBook-Air:assignment-upstream-SS ikyakandula$ git commit -m "completed file structure" 
+Ikyas-MacBook-Air:assignment-upstream-SS ikyakandula$ echo "Snapchat was really changing the game with citizen journalism content this weekend." >> s1/s2/text_chunk.txt
+Ikyas-MacBook-Air:assignment-upstream-SS ikyakandula$ echo "Media is on the cusp of innovation, it's insane." >> s1/s2/text_chunk.txt
+Ikyas-MacBook-Air:assignment-upstream-SS ikyakandula$ echo "Snapchat was really changing the game with citizen jounralism content this weekend." >> s1/s2/Advanced/text_chunk2.txt
+Ikyas-MacBook-Air:assignment-upstream-SS ikyakandula$ echo "Media is on the cusp of innovation, it's insane." >> s1/s2/Advanced/text_chunk2.txt
+Ikyas-MacBook-Air:assignment-upstream-SS ikyakandula$ echo "I can't wait to see what develops soon." >> s1/s2/Advanced/text_chunk2.txt
+Ikyas-MacBook-Air:assignment-upstream-SS ikyakandula$ git add s1 
+Ikyas-MacBook-Air:assignment-upstream-SS ikyakandula$ git commit -m "completed part 1 of assignment"
